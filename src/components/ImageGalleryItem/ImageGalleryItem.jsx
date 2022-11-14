5bf88e30ba82d19d65adf4a067f90c005715c92e
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-14 13:12:57 +0200

    hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './ImageGalleryItem.module.scss';
 import Modal from '../Modal/Modal';
 import PropTypes from 'prop-types';
@@ -15,35 +15,35 @@ import PropTypes from 'prop-types';
 //   );
 // }
 
-class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+function ImageGalleryItem({ id, webformatURL, largeImageURL, tags }) {
+  // state = {
+  //   showModal: false,
+  // };
+
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  render() {
-    const { id, webformatURL, largeImageURL, tags } = this.props;
-    return (
-      <>
-        <li key={id} className={css.item}>
-          <img
-            className={css.img}
-            src={webformatURL}
-            alt={tags}
-            onClick={this.toggleModal}
-          />
-        </li>
-        {this.state.showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt={tags} />
-          </Modal>
-        )}
-      </>
-    );
-  }
+  // const { id, webformatURL, largeImageURL, tags } = this.props;
+  return (
+    <>
+      <li key={id} className={css.item}>
+        <img
+          className={css.img}
+          src={webformatURL}
+          alt={tags}
+          onClick={toggleModal}
+        />
+      </li>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt={tags} />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 ImageGalleryItem.propTypes = {
